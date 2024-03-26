8702db2fcd21a9da3c2ebbf5dd56794114e269aa
Nadya Karaketova <ikoriza@gmail.com>
2024-03-26 13:51:09 +1100

    fix quotes

--- src/games/even.js
+++ src/games/even.js
@@ -1,7 +1,7 @@
 import getRandomIntInclusive from '../lib.js';
 import app from '../index.js';
 
-const TASK_TEXT = 'Answer \'yes\' if the number is even, otherwise answer \'no\'.';
+const TASK_TEXT = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 const isEven = (num) => ((num % 2 === 0) ? 'yes' : 'no');
 
--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const app = (taskText, questionGenerator, answerCalculator) => {
     if (userAnswer === answer) {
       console.log('Correct!');
     } else {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${answer}'.\nLet's try again, ${userName}!`);
+      console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${answer}".\nLet's try again, ${userName}!`);
       return;
     }
   }
