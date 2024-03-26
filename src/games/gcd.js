426d552256bb6f684172f5f7c88b241fcbe1f029
Nadya Karaketova <ikoriza@gmail.com>
2024-03-26 11:35:13 +1100

    fix brain-gcd

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -3,14 +3,14 @@ import app from '../index.js';
 
 const TASK_TEXT = 'Find the greatest common divisor of given numbers.';
 
-const getRandomPair = () => {
-  const number1 = getRandomIntInclusive(1, 100);
-  const number2 = getRandomIntInclusive(1, 100);
-
-  return `${number1} ${number2}`;
-};
+const getRandomPair = () => [getRandomIntInclusive(1, 100), getRandomIntInclusive(1, 100)];
 
 const getGCD = (a, b) => ((b === 0) ? a : getGCD(b, a % b));
+
+const createQuestion = () => {
+  const pair = getRandomPair();
+  return pair.join(' ');
+};
 
 const getAnswer = (str) => {
   const [a, b] = str.split(' ');
@@ -18,4 +18,4 @@ const getAnswer = (str) => {
   return String(answer);
 };
 
-export default () => app(TASK_TEXT, getRandomPair, getAnswer);
+export default () => app(TASK_TEXT, createQuestion, getAnswer);
